e06e57b9a43250385278cc0a1ef6378794d44127
eric chang <urbanapartheid@gmail.com>
2022-02-03 19:31:51 -0500

    fe update for auth

--- src/app/services/data/todo-data.service.ts
+++ src/app/services/data/todo-data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_URL } from 'src/app/app.constants';
 import { Todo } from 'src/app/list-todos/list-todos.component';
 
 @Injectable({
@@ -11,22 +12,22 @@ export class TodoDataService {
   }
 
   retrieveAllTodos(username: string) {
-    return this.http.get<Todo[]>(`http://localhost:8080/users/${username}/todos`);
+    return this.http.get<Todo[]>(`${API_URL}/users/${username}/todos`);
   }
 
   retrieveTodo(username: string, id: number) {
-    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
   }
 
   deleteTodo(username: string, id: number) {
-    return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
+    return this.http.delete(`${API_URL}/users/${username}/todos/${id}`);
   }
 
   updateTodo(username: string, id: number, todo: Todo) {
-    return this.http.put(`http://localhost:8080/users/${username}/todos/${id}`, todo);
+    return this.http.put(`${API_URL}/users/${username}/todos/${id}`, todo);
   }
 
   createTodo(username: string, todo: Todo) {
-    return this.http.post(`http://localhost:8080/users/${username}/todos`, todo);
+    return this.http.post(`${API_URL}/users/${username}/todos`, todo);
   }
 }
